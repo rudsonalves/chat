b814c191c7e4b430587a54352b79c3fb925ff3e8
Rudson R Alves <rudsonalves@yahoo.com.br>
2023-04-14 13:50:08 -0300

    Projeto terminado!

--- lib/pages/auth_or_app_page.dart
+++ lib/pages/auth_or_app_page.dart
@@ -1,6 +1,8 @@
+import 'package:chat/core/services/notification/chat_notification_service.dart';
 import 'package:flutter/material.dart';
 // ignore: depend_on_referenced_packages
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 import '../core/models/chat_user.dart';
 import '../core/services/auth/auth_service.dart';
@@ -13,6 +15,12 @@ class AuthOrAppPage extends StatelessWidget {
 
   Future<void> init(BuildContext context) async {
     await Firebase.initializeApp();
+
+    // ignore: use_build_context_synchronously
+    await Provider.of<ChatNotificationService>(
+      context,
+      listen: false,
+    ).init();
   }
 
   @override
